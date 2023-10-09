ec7279f98bf3cbf019bb68083e90e3bc0169f0ee
BlackCAT <pasankalhara5@gmail.com>
2023-10-09 21:26:06 +0530

    CHG | Configure project

---
+++ src/lib/db/schema/supports.ts
@@ -0,0 +1,25 @@
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+import { mysqlTable, serial, varchar, int } from "drizzle-orm/mysql-core";
+
+export const supports = mysqlTable("supports", {
+	id: serial("id").primaryKey(),
+	userId: varchar("userId", { length: 256 }).notNull(),
+	message: varchar("message", { length: 256 }).notNull(),
+	date: varchar("date", { length: 256 }).notNull(),
+	ip: varchar("ip", { length: 256 }).notNull(),
+	supportedBy: varchar("supportedBy", { length: 256 }).notNull(),
+	supportedDate: varchar("supportedDate", { length: 256 }).notNull(),
+	supportedIp: varchar("supportedIp", { length: 256 }).notNull(),
+	supportedReason: varchar("supportedReason", { length: 256 }).notNull(),
+	supportedStatus: int("supportedStatus").notNull(),
+});
+// Schema for CRUD - used to validate API requests
+export const insertComputerSchema = createInsertSchema(supports);
+export const selectComputerSchema = createSelectSchema(supports);
+export const computerIdSchema = selectComputerSchema.pick({ id: true });
+export const updateComputerSchema = selectComputerSchema;
+
+export type Computer = z.infer<typeof selectComputerSchema>;
+export type NewComputer = z.infer<typeof insertComputerSchema>;
+export type ComputerId = z.infer<typeof computerIdSchema>["id"];
