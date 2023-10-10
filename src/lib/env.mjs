e4425b0a820b6120b6823a40fb2e7f85f3a312bd
BlackCAT <52720489+psnwd@users.noreply.github.com>
2023-10-10 15:18:51 +0530

    FIX | build issue fix

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -14,9 +14,9 @@ export const env = createEnv({
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
+      (str) => process.env.NEXTAUTH_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL_URL ? z.string().min(1) : z.string().url()
+      process.env.NEXTAUTH_URL ? z.string().min(1) : z.string().url()
     ),
     DISCORD_CLIENT_ID: z.string().min(1),
     DISCORD_CLIENT_SECRET: z.string().min(1),
