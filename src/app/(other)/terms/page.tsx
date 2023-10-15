59fd1ffd28adbec8e140b7215ae5c661d12e655a
BlackCAT <pasankalhara5@gmail.com>
2023-10-15 19:01:11 +0530

    CHG | Add contact and layout fixes

--- src/app/(other)/terms/page.tsx
+++ src/app/(other)/terms/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "FiveManager | Support",
+  title: "FiveManager | Terms of Service",
   description:
     "FiveM server management web application for managing fiveM server and players.",
 }
