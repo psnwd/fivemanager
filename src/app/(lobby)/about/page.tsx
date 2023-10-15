0c07be0f8d7eab6d670871fc7df7443371361aba
BlackCAT <pasankalhara5@gmail.com>
2023-10-15 19:06:20 +0530

    FIX | lint fixes

--- src/app/(lobby)/about/page.tsx
+++ src/app/(lobby)/about/page.tsx
@@ -15,9 +15,9 @@ function page() {
         <div>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable.
+          humour, or randomised words which don&apos;t look even slightly believable.
           If you are going to use a passage of Lorem Ipsum, you need to be sure
-          there isn't anything embarrassing hidden in the middle of text. All
+          there isn&apos;t anything embarrassing hidden in the middle of text. All
           the Lorem Ipsum generators on the Internet tend to repeat predefined
           chunks as necessary, making this the first true generator on the
           Internet. It uses a dictionary of over 200 Latin words, combined with
@@ -34,9 +34,9 @@ function page() {
         <div>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable.
+          humour, or randomised words which don&apos;t look even slightly believable.
           If you are going to use a passage of Lorem Ipsum, you need to be sure
-          there isn't anything embarrassing hidden in the middle of text. All
+          there isn&apos;t anything embarrassing hidden in the middle of text. All
           the Lorem Ipsum generators on the Internet tend to repeat predefined
           chunks as necessary, making this the first true generator on the
           Internet. It uses a dictionary of over 200 Latin words, combined with
