ad6f0d80e5474a6e23b5cca1ba3d1a1a63e2cf9c
BlackCAT <pasankalhara5@gmail.com>
2023-10-18 14:51:21 +0530

    db and orm update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
-import type { NextRequest } from "next/server"
-import { NextResponse } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 import { getToken } from "next-auth/jwt"
 
 export default async function middleware(req: NextRequest) {
