7654688fc188b614d28755061396c120941a0878
BlackCAT <pasankalhara5@gmail.com>
2023-10-27 20:43:02 +0530

    CHG | UI updates

--- src/app/(auth)/account/loading.tsx
+++
@@ -1,5 +0,0 @@
-function loading() {
-  return <div>loading</div>
-}
-
-export default loading
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image"
 import { signIn, signOut, useSession } from "next-auth/react"
 
 import { Button } from "../ui/button"
@@ -7,14 +8,26 @@ import { Button } from "../ui/button"
 export default function SignIn() {
   const { data: session, status } = useSession()
 
-  if (status === "loading") return <div>Loading...</div>
+  if (status === "loading")
+    return (
+      <>
+        <div className="absolute left-[48%] top-[48%] animate-ping">
+          <Image
+            src="/images/logo.svg"
+            width={100}
+            height={100}
+            alt="loading image"
+          />
+        </div>
+      </>
+    )
 
   if (session) {
     return (
       <>
         Signed in as {session.user?.email} <br />
         <Button variant={"destructive"} onClick={() => signOut()}>
-          Sign out
+          Log out
         </Button>
       </>
     )
@@ -22,7 +35,7 @@ export default function SignIn() {
   return (
     <>
       Not signed in <br />
-      <Button onClick={() => signIn()}>Sign in</Button>
+      <Button onClick={() => signIn()}>Login</Button>
     </>
   )
 }
